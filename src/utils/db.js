cbc28f163cab861bc5283c5c5047d560b0f10a13
Ngô Bảo Long <ngobaolong0401@gmail.com>
2026-02-27 10:41:12 +0700

    fix(config): load database credentials from environment variables

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,12 +1,22 @@
 import knex from 'knex';
+import dotenv from 'dotenv';
+
+dotenv.config();  
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_PORT = process.env.DB_PORT;
+
 export default knex({
   client: 'pg',
   connection: {
-    host: 'aws-1-ap-southeast-2.pooler.supabase.com',
-    post: 5432,
-    user: 'postgres.oirldpzqsfngdmisrakp',
-    password: 'WYaxZ0myJw9fIbPH',
-    database: 'postgres'
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
   },
   pool: { min: 0, max: 7 }
 });
